b0b59cbfc141857969d19d3fec701336cc9c7dc1
antocas <antocas1990@hotmail.com>
2018-02-20 20:04:17 +0100

    Added User model to the db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_many :articles
 
+  before_save { self.email = email.downcase }
   validates :username, presence: true, length: { minimum: 4, maximum: 15 },
             uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: { case_sensitive: false },
+  validates :email, presence: true, uniqueness: true,
             length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }
 end
