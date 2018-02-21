67e347a8b8d4879b6fe87232f4836b948958d596
antocas <antocas1990@hotmail.com>
2018-02-21 18:54:18 +0100

    UI limitations and minor fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :show]
 
     def index
         @users = User.paginate(page: params[:page], per_page: 10)
@@ -18,11 +19,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             redirect_to articles_path
         else
@@ -31,8 +30,11 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
         @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 
     private
