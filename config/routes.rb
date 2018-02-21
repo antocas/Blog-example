fd36644a532d9839fc093039ce5ce2643f5dae42
antocas <antocas1990@hotmail.com>
2018-02-21 10:17:17 +0100

    Added sign up for new users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get 'about', to: 'welcome#about'
-
+  get 'signup', to: 'users#new'
+  
+  resources :users, except: [:new]
+  
   resources :articles do
   	resources :comments
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220170704) do
+ActiveRecord::Schema.define(version: 20180221081723) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180220170704) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "articles"
