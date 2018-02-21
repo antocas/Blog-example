4492cfd4b11bd54d2007cd7270c0aa14c93d0d37
antocas <antocas1990@hotmail.com>
2018-02-21 13:51:25 +0100

    116 Auth Methods

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'about', to: 'welcome#about'
   get 'signup', to: 'users#new'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :users, except: [:new]
   
   resources :articles do
